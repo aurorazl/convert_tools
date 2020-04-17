0da66a9c5201e68b4f9daf83c80897efbcfd306d
dragon0486 <869071540@qq.com>
2020-04-18 00:51:20 +0800

    parallel cal thr

--- mean_ap.py
+++ mean_ap.py
@@ -383,7 +383,7 @@ def eval_map(det_results,
             mean_ap[thr] = np.array(aps).mean().item() if aps else 0.0
     print("done calculate ious for iou_thr={}.............".format(str(iou_thr)))
     outdata = print_map_summary(
-        mean_ap, eval_results, iou_thr, dataset, area_ranges, logger=logger,category_info)
+        mean_ap, eval_results, iou_thr, dataset, area_ranges, logger=logger,category_info=category_info)
 
     return mean_ap, outdata,det_iou
 
